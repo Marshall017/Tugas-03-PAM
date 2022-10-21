432c1caabf408efddac77f165fe5c64838ed6b61
Marshallramdh <marshall.120140017@student.itera.ac.id>
2022-10-21 22:23:56 +0700

    init

---
+++ component/data.js
@@ -0,0 +1,48 @@
+const DataPenerbangan = [
+  [
+    "Bandung",
+    "Lampung",
+    "Jawa",
+    "Ambon",
+    "Aceh",
+    "Jakarta",
+    "Jambi",
+    "Maluku",
+    "Pontianak",
+    "Samarinda",
+  ],
+  [
+    "Lampung",
+    "Bandung",
+    "Ambon",
+    "Jawa",
+    "Samarinda",
+    "Maluku",
+    "Pontianak",
+    "Jakarta",
+    "Jambi",
+    "Aceh",
+  ],
+  [
+    "11 Desember 2022",
+    "11 Desember 2022",
+    "11 Desember 2022",
+    "11 Desember 2022",
+    "11 Desember 2022",
+    "11 Desember 2022",
+  ],
+  [
+    "Garuda Indonesia",
+    "Lion Air",
+    "Batik Air",
+    "Garuda Indonesia",
+    "Lion Air",
+    "Batik Air",
+    "Batik Air",
+    "Garuda Indonesia",
+    "Lion Air",
+    "Batik Air",
+  ],
+];
+
+export default DataPenerbangan;
